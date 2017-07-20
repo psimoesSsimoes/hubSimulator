11f040207e92dca649f4f1af13005d426c1eadaa
psimoes <p.simoes@campus.fct.unl.pt>
2017-07-20 14:46:47 +0100

    before i started touching hubSimulator

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+
+//serve static assets from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +18,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(9005, function(){
+  console.log('listening on *:9005');
 });
